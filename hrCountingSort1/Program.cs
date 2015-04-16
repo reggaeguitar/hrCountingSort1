8b7b92af593068ef2f0e9510fe40fbe4abb23b95
reggaeguitar <reggaeguitar@users.noreply.github.com>
2015-04-16 15:49:49 -0500

    Initial commit

---
+++ hrCountingSort1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace hrCountingSort1
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main()
+        {
+            var numItems = Int32.Parse(Console.ReadLine());
+            var counts = new int[100];
+            var nums = Console.ReadLine().Split(' ').Select(Int32.Parse);
+            foreach (var num in nums)
+            {
+                ++counts[num];
+            }
+            var sb = new StringBuilder();
+            foreach (var count in counts)
+            {
+                sb.Append(count);
+                sb.Append(" ");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
